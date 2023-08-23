65fb68c93d7ac4834f238abdaf8a3888c3027068
Marko Nikolaš <marko.nikolas@news.co.uk>
2023-08-23 13:22:47 +0200

    Untrack build

--- build/dynamic/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '4f56fa2e42f0dd653f1d');
--- build/static/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fed2675299348a1eda7d');
